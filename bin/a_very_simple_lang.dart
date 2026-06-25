345799d095c96dea49417e74a36489a4d27afaee
Pradeep Sharma <ironpradeep991@gmail.com>
2026-06-25 20:39:37 +0530

    usable

--- bin/a_very_simple_lang.dart
+++ bin/a_very_simple_lang.dart
@@ -1,13 +1,30 @@
+import 'package:a_very_simple_lang/expression.dart';
 import 'package:a_very_simple_lang/lexer.dart';
+import 'package:a_very_simple_lang/parser.dart';
+import 'package:a_very_simple_lang/token.dart';
 
 void main(List<String> arguments) {
-  Lexer lexer = Lexer(input: "22+3/4*10+(11+6)+66");
-
   try {
-    print(lexer.scanTokens().toString());
+    // E -> E ( '+' | '-' T )*
+    // T -> F ( '*' | '/' F )*
+    // F -> Integer | '(' E ')'
+
+    // E => Expression
+    // T => Term
+    // F => Factor
+
+    Lexer lexer = Lexer(input: "6+(2*3)/4");
+    List<Token> tokens = lexer.scanTokens();
+    print(tokens.toString());
+    Parser parser = Parser(tokenList: tokens);
+    Expression expression = parser.parse();
+    print(expression.toString());
   } catch (e) {
     if (e is LexerError) {
       print("${e.errorMessage} val:${e.val}");
+    }
+    if (e is ParserError) {
+      print("${e.errorMessage} val:${e.token}");
     }
   }
 }
