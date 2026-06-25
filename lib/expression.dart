c37038e38e0bdcdaf6cfa3fd13d8f082f514bd81
Pradeep Sharma <ironpradeep991@gmail.com>
2026-06-25 15:43:55 +0530

    expression added

---
+++ lib/expression.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:a_very_simple_lang/token.dart';
+
+abstract class Expression {}
+
+class NumberLiteral extends Expression {
+  final int val;
+  NumberLiteral({required this.val});
+}
+
+class BinaryOperator extends Expression {
+  final Expression left;
+  final Expression right;
+  final Token operator;
+
+  BinaryOperator({
+    required this.left,
+    required this.right,
+    required this.operator,
+  });
+}
